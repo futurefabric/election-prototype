1dbf929a073972cb8e2d9481cb83034ba9a40c8e
Anna Powell-Smith <annapowellsmith@gmail.com>
2014-11-19 18:07:38 +0000

    Tidy up routes a bit

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,26 @@ module.exports = {
                 {'assetPath' : assetPath});
     });
 
+    // Archiving search
+    // How GOV.UK search might work for archived topics
+    app.get('/search/archive', function (req, res) {
+      res.render('archive-search', {'assetPath' : assetPath});
+    });
+
+    // Archiving prototypes (Governors' handbook)
+    app.get('/governors-handbook', function (req, res) {
+      res.render('governors-handbook', {'assetPath' : assetPath});
+    });
+    app.get('/info/governors-handbook', function (req, res) {
+      res.render('governors-handbook-info', {'assetPath' : assetPath});
+    });
+    app.get('/governors-handbook/v1', function (req, res) {
+      res.render('governors-handbook-version', {'assetPath' : assetPath});
+    });
+
     // Archiving prototypes (Building services)
+    // Click on linked page, goes through to 410 page, you can then
+    // see the inverted page.
     app.get('/archive', function (req, res) {
       res.render('archive-1', {'assetPath' : assetPath});
     });
@@ -23,7 +42,9 @@ module.exports = {
       res.render('archive-5', {'assetPath' : assetPath});
     });
 
-    // Archiving prototypes (G-Cloud)
+    // Archiving and 410 prototypes (G-Cloud)
+    // Google search, Google results with archived link,
+    // goes through to 410 page, you can then see the inverted page
     app.get('/google', function (req, res) {
       res.render('archive/google-search', {'assetPath' : assetPath});
     });
@@ -37,7 +58,14 @@ module.exports = {
       res.render('archive/cloudstore', {'assetPath' : assetPath});
     });
 
-    // Policy prototypes (topics view)
+    // Policy areas with new list of names
+    app.get('/policies-renamed', function (req, res) {
+      res.render('policies-renamed', {'assetPath' : assetPath});
+    });
+
+    // Policy prototypes (Crime and policing)
+    // List of policy areas, filter for "crime", then go through to
+    // the "window" of latest stuff
     app.get('/policies', function (req, res) {
       res.render('policies/topics.html', {'assetPath' : assetPath});
     });
@@ -48,7 +76,12 @@ module.exports = {
       res.render('policies/crime-and-policing-latest.html', {'assetPath' : assetPath});
     });
 
-    // Taxonomy prototypes (Early years childcare)
+    // Policy prototypes (Early years childcare)
+    // List of topics, then go through to early years childcare page with
+    // a "window" at the bottom
+    // If you click on any of the "see more" links you get to
+    // example 3, which is a filtered publication list by policy topic
+    // Then example 4 is just the window, without any of the other content
     app.get('/schools-colleges', function (req, res) {
       res.render('example-0', {'assetPath' : assetPath });
     });
@@ -74,21 +107,26 @@ module.exports = {
       res.render('example-4', {'assetPath' : assetPath });
     });
 
+    // New "scheme page"
+    app.get('/scheme', function (req, res) {
+      res.render('scheme', {'assetPath' : assetPath });
+    });
+
     // Logging prototypes.
     app.get('/logging', function (req, res) {
-      res.render('logging-1', {'assetPath' : assetPath});
+      res.render('logging/logging-1', {'assetPath' : assetPath});
     });
     app.get('/logging-async', function (req, res) {
-      res.render('logging-2', {'assetPath' : assetPath});
+      res.render('logging/logging-2', {'assetPath' : assetPath});
     });
     app.get('/logging-reordered', function (req, res) {
-      res.render('logging-3', {'assetPath' : assetPath});
+      res.render('logging/logging-3', {'assetPath' : assetPath});
     });
     app.get('/logging-async-filament', function (req, res) {
-      res.render('logging-4', {'assetPath' : assetPath});
+      res.render('logging/logging-4', {'assetPath' : assetPath});
     });
     app.get('/logging-async-filament-with-reorder', function (req, res) {
-      res.render('logging-5', {'assetPath' : assetPath});
+      res.render('logging/logging-5', {'assetPath' : assetPath});
     });
 
   }
